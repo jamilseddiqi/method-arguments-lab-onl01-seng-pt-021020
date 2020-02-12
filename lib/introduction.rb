a69e610b7e8d8dc2d4d7b7aa0a42f16077cc7b11
Jamil Seddiqi <jamilsediq@gmail.com>
2020-02-12 05:00:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,6 @@ def introduction(name)
 end
 
 def introduction(name, language)
-  puts "Hi, my name is #{name} and I speak #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
-name` and `language` and outputs the phrase: `"Hi, my name is #{name} and I am
-learning to program in #{language}."
