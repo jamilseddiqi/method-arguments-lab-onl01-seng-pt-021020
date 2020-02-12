1cd565a62be6af6a12cbceccdcacecbc8c0ebe89
Jamil Seddiqi <jamilsediq@gmail.com>
2020-02-12 05:06:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,8 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-introduction("Jamil")
+
 
 def introduction_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-introduction("Jamil", "Ruby")
